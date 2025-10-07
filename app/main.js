b564d7555f274d6e8bdf70b360b6f42660741e5f
Harshita <hrst944@gmail.com>
2025-10-07 16:13:53 +0530

    [any message]

--- app/main.js
+++ app/main.js
@@ -5,10 +5,22 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-   socket.write("HTTP/1.1 200 OK\r\n\r\n")
-   socket.on("close", () => {
-     socket.end();
-   });
+   
+  socket.on("data",(data)=>{
+    const request =data.toString();
+    console.log(request);
+
+    const path =getPath(request);
+    console.log(path);
+
+    let response;;
+    if(path==="/"){
+      response ="HTTP/1.1 200 OK\r\n\r\n";
+    }else{
+      response="HTTP/1.1 404 Not Found\r\n\r\n";
+    }
+    socket.write(response);
+    })
  });
 
 server.listen(4221, "localhost");
